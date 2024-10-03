294647fa1c24beeecdde5b5304559e6dc0747d89
NaMinhyeok <nmh9097@gmail.com>
2024-10-03 22:21:40 +0900

    initial commit

---
+++ src/main/java/minhyeok/taskschedulingpractice/TaskschedulingPracticeApplication.java
@@ -0,0 +1,13 @@
+package minhyeok.taskschedulingpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskschedulingPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskschedulingPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/minhyeok/taskschedulingpractice/TaskschedulingPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package minhyeok.taskschedulingpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskschedulingPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
